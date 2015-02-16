8c3f719bf3539b480d5ea664ac626e03c6876f01
ImbellicusAnimo <crizz@imbellicus-animo.org>
2015-02-16 18:01:24 +0100

    Game Manager
    Writing the Game Manager

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -35,7 +35,7 @@ public class BoardManager : MonoBehaviour {
 		gridPositions.Clear();
 		
 		for (int x = 1; x < columns - 1; x++) {
-			for (int y = 1; y < rows -1; y++) {
+			for (int y = 1; y < rows - 1; y++) {
 				gridPositions.Add(new Vector3(x,y,0f));
 			}
 		}
@@ -44,8 +44,8 @@ public class BoardManager : MonoBehaviour {
 	void BoardSetup() {
 		boardHolder = new GameObject("Board").transform;
 
-		for(int x = 1; x < columns + 1; x++) {
-			for(int y = 1; y < rows + 1; y++) {
+		for(int x = -1; x < columns + 1; x++) {
+			for(int y = -1; y < rows + 1; y++) {
 				GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
 				if(x == -1 || x == columns || y == -1 || y == rows) {
 					toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
